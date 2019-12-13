0f68819b38c771c7f61abf375360e9d4a0e53c2e
manish k <man@mani.com>
2019-12-13 12:16:50 +0530

    added parametrisation with hashTable

--- src/test/java/testcases/TestNGParamaterizationExcel.java
+++ src/test/java/testcases/TestNGParamaterizationExcel.java
@@ -1,5 +1,8 @@
 package testcases;
 
+import java.util.Hashtable;
+
+import org.apache.poi.hwpf.usermodel.Table;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -8,8 +11,8 @@ public class TestNGParamaterizationExcel {
 	public static ExcelReader excel=null;
 	
 	@Test (dataProvider = "getData")
-	public void testData(String username, String password, String is_correct) {
-		System.out.println(username + "----" +password + "----" +is_correct);
+	public void testData(Hashtable<String, String> data) {
+		System.out.println(data.get("username") + "----" +data.get("password") + "----" +data.get("is_correct"));
 	}
 	
 	@DataProvider
@@ -24,11 +27,17 @@ public class TestNGParamaterizationExcel {
 		int cols=excel.getColumnCount(sheetname);
 		
 		
-		Object[][] data=new Object[rows-1][cols];
+		Object[][] data=new Object[rows-1][1];
+		Hashtable<String, String> table=null;
+		
 		for (int rowNum=2;rowNum<=rows;rowNum++) {
+			table=new Hashtable<String, String>();
+			
 			for (int colNum=0;colNum<cols;colNum++) {
-				data[rowNum-2][colNum]=excel.getCellData(sheetname, colNum, rowNum);
-				System.out.println(data[rowNum-2][colNum]);
+				//data[rowNum-2][colNum]=excel.getCellData(sheetname, colNum, rowNum);
+				table.put(excel.getCellData(sheetname, colNum, 1), excel.getCellData(sheetname, colNum, rowNum));
+			
+				data[rowNum-2][0]=table;
 			}
 		}
 		
